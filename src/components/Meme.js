5f59a3adf59fc1d2821c18308ddcbbbd4fd93eaf
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-01-17 08:35:32 +0100

    state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import memesData from "../memesData"
 
 export default function Meme() {
+  // const [memeImage, setMemeImage] = useState('')
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: ""
+
+  })
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
+  function showMeme(e) {
+    e.preventDefault();
+    const memesArray = allMemeImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
+    // const url = memesArray[randomNumber].url
+    
+   
+  }
   return (
     <main>
       <form className='form'>
@@ -14,8 +36,9 @@ export default function Meme() {
           placeholder='Bottom text'
           className='form-inputs'
         />
-        <button className='form-button'><img src={require("../images/meme-image.png")} alt="" id='img'/></button>
+        <button className='form-button' onClick={showMeme}><img src={require("../images/meme-image.png")} alt="" id='img'/></button>
       </form>
+      <img src={meme.randomImage} alt="" className='meme-image'/>
 
     </main>
   )
